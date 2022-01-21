aee6ab83e1ddeb18d76dba8f71af2079aafd06fb
tranthivi <Tranthivi2k@gmail.com>
2022-01-22 00:07:57 +0700

    update SanPhamController

--- eShop/Controllers/SanPhamController.cs
+++ eShop/Controllers/SanPhamController.cs
@@ -6,6 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using eShop.Entities;
 using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
 
 namespace eShop.Controllers
 {
@@ -33,5 +37,63 @@ namespace eShop.Controllers
 
             return hoso;
         }
+        private readonly IConfiguration _configuration;
+        public SanPhamController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        // GET: api/SanPham/
+        public JsonResult Get()
+        {
+            string query = @"
+                        select * from SanPham";
+            DataTable table = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(SqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        // POST api/sanpham
+        [HttpPost]
+        public JsonResult Post(SanPham sp)
+        {
+            string query = @"
+                        insert into SanPham ( TenSP, GiaSP, DonViTinh, MoTa, SoLuongTon, LoaiSanPhamIDLoaiSP) 
+                        values (@TenSP, @GiaSP, @DonViTinh, @MoTa, @SoLuongTon, @LoaiSanPhamId)";
+            DataTable table = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(SqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@TenSP", sp.TenSP);
+                    myCommand.Parameters.AddWithValue("@GiaSP", sp.GiaSP);
+                    myCommand.Parameters.AddWithValue("@DonViTinh", sp.DonViTinh);
+                    myCommand.Parameters.AddWithValue("@MoTa", sp.MoTa);
+                    myCommand.Parameters.AddWithValue("@SoLuongTon", sp.SoLuongTon);
+                    myCommand.Parameters.AddWithValue("@LoaiSanPhamId", sp.LoaiSanPhamId);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            return new JsonResult("success");
+        }
+
     }
 }
