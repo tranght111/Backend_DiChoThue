29aca5a5f116e8f29fc962718d7b3f3aead59964
tranthivi <50940106+tranthivi@users.noreply.github.com>
2022-01-23 09:53:41 +0700

    update Api SanPham

---
+++ eShop/Controllers/ImageSanPhamController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using eShop.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+
+namespace eShop.Controllers
+{
+    [Route("api/productimage/url")]
+    [ApiController]
+    public class ImageSanPhamController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public ImageSanPhamController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        // GET: api/productimage
+        public JsonResult Get()
+        {
+            string query = @"
+                        select UrlImage from ImageSanPham";
+            DataTable table = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(SqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        
+    }
+}
