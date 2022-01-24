03542259ee45f0ffb0ebff148d2a17ca6a6479c5
nhanlang123 <87190262+nhanlang123@users.noreply.github.com>
2022-01-24 22:59:06 +0700

    Update DoiMatKhauController.cs

--- eShop/Controllers/DoiMatKhauController.cs
+++ eShop/Controllers/DoiMatKhauController.cs
@@ -11,5 +11,35 @@ namespace eShop.Controllers
     [ApiController]
     public class DoiMatKhauController : ControllerBase
     {
+    private readonly IConfiguration _configuration;
+        public DoiMatKhauController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public JsonResult Put(NguoiDung ng)
+        {
+            string query = @"
+                        update NguoiDung set Pass = @NewPass where Username = @username and Pass=@Pass";
+            DataTable table = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(SqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@NewPass", ng.New_Password);
+                    myCommand.Parameters.AddWithValue("@Pass", ng.Password);
+                    myCommand.Parameters.AddWithValue("@username", ng.Username);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            return new JsonResult("success");
+        }
+    }
     }
 }
