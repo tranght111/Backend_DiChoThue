1411104e80ff3af2c98e44c02e316b7afa2aa671
nhanlang123 <87190262+nhanlang123@users.noreply.github.com>
2022-01-25 15:12:32 +0700

    Update TinhLuongShipperController.cs

--- eShop/Controllers/TinhLuongShipperController.cs
+++ eShop/Controllers/TinhLuongShipperController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using eShop.Entities;
 
 namespace eShop.Controllers
 {
@@ -11,5 +15,36 @@ namespace eShop.Controllers
     [ApiController]
     public class TinhLuongShipperController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+        public TinhLuongShipperController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        // Post: api/TinhLuongShipper/
+        public JsonResult Post(LuongShipper lshipper)
+        {
+            string query = @" exec TinhLuongShipper @IDShipper, @Thang, @Nam";
+            DataTable table = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(SqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@IDShipper", lshipper.ShipperId);
+                    myCommand.Parameters.AddWithValue("@Thang", lshipper.Thang);
+                    myCommand.Parameters.AddWithValue("@Nam", lshipper.Nam);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
     }
 }
