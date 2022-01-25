71e069c406f6c87711699455d78a28e51ba355b5
lqtram <50936265+lqtram@users.noreply.github.com>
2022-01-25 10:51:12 +0700

    Cập nhật Trạng thái đơn hàng + Thêm, Xóa, Sửa Đánh giá sản phẩm

---
+++ eShop/Controllers/CapNhatGiaoHangThanhCongController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using eShop.Entities;
+
+namespace eShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CapNhatGiaoHangThanhCongController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public CapNhatGiaoHangThanhCongController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+
+
+        [HttpGet]
+        // GET: api/CapNhatGiaoHangThanhCong/
+        public JsonResult Get()
+        {
+            string query = @"
+                        select * from TrangThaiDonHang";
+            DataTable table = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(SqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        // POST api/CapNhatGiaoHangThanhCong
+        [HttpPost]
+        public JsonResult Post(TrangThaiDonHang ttdh)
+        {
+            string query = @"
+                        insert into TrangThaiDonHang (NgayCapNhat, TrangThai, DonHangId) 
+                        values (getdate(), 'Giao hàng thành công', @DonHangId)";
+            DataTable table = new DataTable();
+            string SqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+            SqlDataReader myReader;
+            using (SqlConnection myConn = new SqlConnection(SqlDataSource))
+            {
+                myConn.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myConn))
+                {
+                    myCommand.Parameters.AddWithValue("@DonHangId", ttdh.DonHangId);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myConn.Close();
+                }
+            }
+            return new JsonResult("success");
+        }
+    }
+}
