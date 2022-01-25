fc0d0aba480534837625c10c3dff7c99c8ec9cd9
nhanlang123 <87190262+nhanlang123@users.noreply.github.com>
2022-01-25 09:46:27 +0700

    Update DatHangController.cs

--- eShop/Controllers/DatHangController.cs
+++ eShop/Controllers/DatHangController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
@@ -21,12 +21,10 @@ namespace eShop.Controllers
             _configuration = configuration;
         }
         [HttpPost]
-        // GET: api/DatHang/
+        // Post: api/DatHang/
         public JsonResult Post(DonHang dh)
         {
-            string query = @"
-                        insert into DonHang(NgayDat, PhiShip, TongTien, HinhThucThanhToan, IDNguoiBan, IDKhachHang) 
-                        values(getdate(), @PhiShip, @TongTien, @HinhThucThanhToan, @IDNguoiBan, @IDKhachHang)";
+            string query = @" exec DatHang @PhiShip, @HinhThucThanhToan, @IDNguoiBan, @IDKhachHang,@SoLuong, @IDSanPham";
             DataTable table = new DataTable();
             string SqlDataSource = _configuration.GetConnectionString("DefaultConnection");
             SqlDataReader myReader;
@@ -36,10 +34,11 @@ namespace eShop.Controllers
                 using (SqlCommand myCommand = new SqlCommand(query, myConn))
                 {
                     myCommand.Parameters.AddWithValue("@PhiShip", dh.PhiShip);
-                    myCommand.Parameters.AddWithValue("@TongTien", dh.TongTien);
                     myCommand.Parameters.AddWithValue("@HinhThucThanhToan", dh.HinhThucThanhToan);
                     myCommand.Parameters.AddWithValue("@IDNguoiBan", dh.NguoiBanId);
                     myCommand.Parameters.AddWithValue("@IDKhachHang", dh.KhachHangId);
+                    myCommand.Parameters.AddWithValue("@SoLuong", dh.SoLuong);
+                    myCommand.Parameters.AddWithValue("@IDSanPham", dh.SanPhamId);
                     myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
                     myReader.Close();
@@ -47,31 +46,10 @@ namespace eShop.Controllers
                 }
             }
 
-            string query2 = @"
-                        insert into ChiTietDonHang(SoLuong, IDDonHang, IDSanPham) 
-                        values(@SoLuong, @IDDonHang, @IDSanPham)";
-            DataTable table2 = new DataTable();
-            string SqlDataSource2 = _configuration.GetConnectionString("DefaultConnection");
-            SqlDataReader myReader2;
-            using (SqlConnection myConn = new SqlConnection(SqlDataSource2))
-            {
-                myConn.Open();
-                using (SqlCommand myCommand = new SqlCommand(query2, myConn))
-                {
-                    myCommand.Parameters.AddWithValue("@SoLuong", dh.chitietdh.SoLuong);
-                    myCommand.Parameters.AddWithValue("@IDSanPham", dh.chitietdh.SanPhamId);
-                    myCommand.Parameters.AddWithValue("@IDDonHang", dh.DonHangId);
-                    myReader2 = myCommand.ExecuteReader();
-                    table2.Load(myReader2);
-                    myReader2.Close();
-                    myConn.Close();
-                }
-            }
-
             return new JsonResult("sucess");
         }
-        [HttpGet] // xem ho so don hang
-
+        /*[HttpGet] // xem don hang
+        
         public JsonResult Get()
         {
             string query = @"
@@ -94,6 +72,6 @@ namespace eShop.Controllers
                 }
             }
             return new JsonResult(table);
-        }
+        }*/
     }
 }
